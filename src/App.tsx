61c9beac5a3f8aac9e52a8fb65a30a94cb76a8d4
Dreamchaser-aus <dreamchaseraus8@gmail.com>
2025-07-06 17:50:36 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Game } from './components/Game';
+const tg = (window as any).Telegram?.WebApp;
+const telegramId = tg?.initDataUnsafe?.user?.id;
+
 
 function App() {
   return <Game />;
